0c998f4d1731f371f7c3145d4b56b17fc9aade86
Ubuntu <ubuntu@ip-10-0-0-17.us-west-2.compute.internal>
2019-09-30 19:31:05 +0000

    add notes details

--- url_7z_xml_to_s3/transfer_to_s3.py
+++ url_7z_xml_to_s3/transfer_to_s3.py
@@ -5,26 +5,25 @@ import subprocess
 import boto3
 import wget
 import os
-#import shutil
 from extract_url import extract_url
 
 '''
-This script ...
+This script downloads the 7z files of Stack Overflow contents and extracts into xml files.
 
 '''
 
 # Create an S3 client
-s3 = boto3.client('s3')
+# s3 = boto3.client('s3')
 
 # Create an S3 bucket
-bucket_name = 'stack-overflow-datadump-xml'
+# bucket_name = 'stack-overflow-datadump-xml'
 
 ### run the 3 lines below only once when creating new S3 bucket
 ##region='us-west-2'
 ##location = {'LocationConstraint': region}
 ##s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
 
-# Files to be uploaded
+
 source_url = 'https://archive.org/download/stackexchange/'
 file_names = extract_url(source_url)
 
@@ -36,8 +35,7 @@ for filename in file_names:
     wget.download(url)
     process_7z_file_shell = "./process_7z_file_batch.sh"
     subprocess.call([process_7z_file_shell], shell=True)
+    # use AWS CLI to bulk upload instead of boto3
     #print('Uploading ' + dir_name + ' to S3...')
     #s3.upload_file(dir_name/*.xml, bucket_name, filename)
-    os.remove(filename) 
-   
-
+    os.remove(filename)
